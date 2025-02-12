adb682697aa5bed465880d88e0b40a6a47238693
Santiago De Leon <santiago.dem@upb.edu.co>
2025-02-12 09:34:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,160 @@
+# coding=utf-8
+
+print('Registro huella de Carbono')
+print()
+print('Cuantos recorridos realizo durante el dia?')
+print()
+Recorridos_hechos = float(input('Ingrese los recorridos hechos: '))
+print()
+
+if Recorridos_hechos< 12:
+    print('Los recorridos minimos tienen que ser 12 durante el dia')
+
+
+elif Recorridos_hechos<=11:
+    print('Ingrese de nuevo la cantidad de recorridos hechos')
+Recorridos_hechos = float(input('Ingrese los recorridos hechos nuevamente: '))
+print()
+
+
+
+
+
+
+
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el primer servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida<=4:
+    print('Ingrese de nuevo la distancia recorrida')
+distancia_recorrida = float(input('Ingrese la distancia recorrida nuevamente: '))
+print()
+
+else: 
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el segundo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el tercer servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el cuarto servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el quinto servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el sexto servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el septimo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el octavo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el noveno servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el decimo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el undecimo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+print()
+
+distancia_recorrida = float(input('Ingrese la distancia recorrida en el ultimo servicio: '))
+print()
+if distancia_recorrida < 5:
+  print('Su registro no es valido y no se tendra en cuenta en el conteo del servicio')
+
+elif distancia_recorrida > 5:
+  print('Su registro es valido y se tendra en cuenta en el conteo del servicio')
+
+
+
+
+
+
+
+
+
+
+
+
